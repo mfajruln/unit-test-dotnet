9a50a2af58f587f66039e65887c01e16e49848e6
Fajrul <fajrul.nugraha@bithealth.co.id>
2024-04-23 09:13:32 +0700

    feat(unit test): adding unit test
    adding unit test in every services on this project

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+{
+            if (candidate == 1)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Not fully implemented.");
+        }
+    }
+}
+
+// sqp_a880790c213e6a37658b5c008234191529fdd18e
